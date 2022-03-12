b93d2aa5a5e516f3f7946ad78c98faf8d4332e15
Максим Глинкин <maximglin05@yandex.ru>
2022-03-12 03:54:39 +0300

    ecp private public check

--- ArchitectZad1WPF/MainWindow.xaml.cs
+++ ArchitectZad1WPF/MainWindow.xaml.cs
@@ -17,6 +17,8 @@ using System.Net.Mail;
 using Limilabs.Client.IMAP;
 using Limilabs.Mail;
 using Microsoft.Win32;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Cryptography;
 
 namespace ArchitectZad1WPF
 {
@@ -28,6 +30,205 @@ namespace ArchitectZad1WPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private static X509Certificate2 GetCertificateFromStore(string certName)
+        {
+
+            // Get the certificate store for the current user.
+            X509Store store = new X509Store(StoreLocation.CurrentUser);
+            try
+            {
+                store.Open(OpenFlags.ReadOnly);
+
+                // Place all certificates in an X509Certificate2Collection object.
+                var cert = store.Certificates.Cast<X509Certificate2>().FirstOrDefault(cert => cert.SubjectName.Name.Contains(certName));
+
+                if (cert == null)
+                    throw new Exception("Нет сертификата с таким именем владельца в названии!");
+
+                return cert;
+            }
+            finally
+            {
+                store.Close();
+            }
+        }
+        private static byte[] GetSignedHash(byte[] file, RSA rsaPrivateKey)
+        {
+            byte[] encryptedHash;
+            using (Aes aes = Aes.Create())
+            {
+                // Create instance of Aes for
+                // symetric encryption of the data.
+                aes.KeySize = 256;
+                aes.Mode = CipherMode.CBC;
+                using (ICryptoTransform transform = aes.CreateEncryptor())
+                {
+                    byte[] keyEncrypted = rsaPrivateKey.Encrypt(aes.Key, RSAEncryptionPadding.Pkcs1);
+
+                    // Create byte arrays to contain
+                    // the length values of the key and IV.
+                    byte[] LenK = new byte[4];
+                    byte[] LenIV = new byte[4];
+
+                    int lKey = keyEncrypted.Length;
+                    LenK = BitConverter.GetBytes(lKey);
+                    int lIV = aes.IV.Length;
+                    LenIV = BitConverter.GetBytes(lIV);
+
+                    // Write the following to the FileStream
+                    // for the encrypted file (outFs):
+                    // - length of the key
+                    // - length of the IV
+                    // - ecrypted key
+                    // - the IV
+                    // - the encrypted cipher content
+
+                    SHA256 sha = SHA256.Create();
+                    byte[] hash = sha.ComputeHash(file);
+                    using (MemoryStream outFs = new MemoryStream())
+                    {
+
+                        outFs.Write(LenK, 0, 4);
+                        outFs.Write(LenIV, 0, 4);
+                        outFs.Write(keyEncrypted, 0, lKey);
+                        outFs.Write(aes.IV, 0, lIV);
+
+                        // Now write the cipher text using
+                        // a CryptoStream for encrypting.
+                        using (CryptoStream outStreamEncrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                        {
+                            // blockSizeBytes can be any arbitrary size.
+                            int blockSizeBytes = aes.BlockSize / 8;
+
+                            outStreamEncrypted.Write(hash, 0, hash.Length);
+
+                            outStreamEncrypted.FlushFinalBlock();
+                            outStreamEncrypted.Close();
+                        }
+                        encryptedHash = outFs.ToArray();
+                        outFs.Close();
+                    }
+                }
+            }
+            return encryptedHash;
+        }
+
+
+        private static bool CheckSignWithPublicKey(byte[] file, byte[] encryptedHash, RSA rsaPublicKey) 
+        {
+            SHA256 sha = SHA256.Create();
+            byte[] hash = sha.ComputeHash(file);
+            byte[] decryptedHash;
+
+            // Create instance of Aes for
+            // symetric decryption of the data.
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.Mode = CipherMode.CBC;
+
+                // Create byte arrays to get the length of
+                // the encrypted key and IV.
+                // These values were stored as 4 bytes each
+                // at the beginning of the encrypted package.
+                byte[] LenK = new byte[4];
+                byte[] LenIV = new byte[4];
+
+
+                // Use FileStream objects to read the encrypted
+                // file (inFs) and save the decrypted file (outFs).
+                using (MemoryStream inFs = new MemoryStream(encryptedHash))
+                {
+
+                    inFs.Seek(0, SeekOrigin.Begin);
+                    inFs.Seek(0, SeekOrigin.Begin);
+                    inFs.Read(LenK, 0, 3);
+                    inFs.Seek(4, SeekOrigin.Begin);
+                    inFs.Read(LenIV, 0, 3);
+
+                    // Convert the lengths to integer values.
+                    int lenK = BitConverter.ToInt32(LenK, 0);
+                    int lenIV = BitConverter.ToInt32(LenIV, 0);
+
+                    // Determine the start position of
+                    // the cipher text (startC)
+                    // and its length(lenC).
+                    int startC = lenK + lenIV + 8;
+                    int lenC = (int)inFs.Length - startC;
+
+                    // Create the byte arrays for
+                    // the encrypted Aes key,
+                    // the IV, and the cipher text.
+                    byte[] KeyEncrypted = new byte[lenK];
+                    byte[] IV = new byte[lenIV];
+
+                    // Extract the key and IV
+                    // starting from index 8
+                    // after the length values.
+                    inFs.Seek(8, SeekOrigin.Begin);
+                    inFs.Read(KeyEncrypted, 0, lenK);
+                    inFs.Seek(8 + lenK, SeekOrigin.Begin);
+                    inFs.Read(IV, 0, lenIV);
+
+                    // Use RSA
+                    // to decrypt the Aes key.
+                    byte[] KeyDecrypted = rsaPublicKey.Decrypt(KeyEncrypted, RSAEncryptionPadding.Pkcs1);
+                    
+
+                    // Decrypt the key.
+                    using (ICryptoTransform transform = aes.CreateDecryptor(KeyDecrypted, IV))
+                    {
+
+                        // Decrypt the cipher text from
+                        // from the FileSteam of the encrypted
+                        // file (inFs) into the FileStream
+                        // for the decrypted file (outFs).
+                        using (MemoryStream outFs = new MemoryStream())
+                        {
+
+                            int count = 0;
+
+                            int blockSizeBytes = aes.BlockSize / 8;
+                            byte[] data = new byte[blockSizeBytes];
+
+                            // By decrypting a chunk a time,
+                            // you can save memory and
+                            // accommodate large files.
+
+                            // Start at the beginning
+                            // of the cipher text.
+                            inFs.Seek(startC, SeekOrigin.Begin);
+                            using (CryptoStream outStreamDecrypted = new CryptoStream(outFs, transform, CryptoStreamMode.Write))
+                            {
+                                do
+                                {
+                                    count = inFs.Read(data, 0, blockSizeBytes);
+                                    outStreamDecrypted.Write(data, 0, count);
+                                }
+                                while (count > 0);
+
+                                outStreamDecrypted.FlushFinalBlock();
+                                outStreamDecrypted.Close();
+                            }
+
+                            decryptedHash = outFs.ToArray();
+                            outFs.Close();
+                        }
+                        inFs.Close();
+                    }
+                }
+            }
+
+            if (decryptedHash.Length != hash.Length)
+                return false;
+
+            for(int i = 0; i < decryptedHash.Length; i++)
+                if(decryptedHash[i] != hash[i])
+                    return false;
+
+            return true;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -67,30 +268,40 @@ namespace ArchitectZad1WPF
 
                 if (File.Exists(file.AbsolutePath))
                 {
-                    Cryption cr = new(ecp.Text);
-                    var cert = cr.MyCertificate();
+                    var cert = GetCertificateFromStore(ecp.Text);
 
                     byte[] data = File.ReadAllBytes(file.AbsolutePath);
-                    var signed_data = cr.SignByCertificate(data, cert);
-                    var signed_and_coded_data = cr.Base64Encode(signed_data);
 
+                    
 
-                    System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                    var ms = new MemoryStream();
 
-                    ms.Write(signed_and_coded_data, 0, signed_and_coded_data.Length);
+                    ms.Write(data, 0, data.Length);
                     ms.Position = 0;
 
-                    System.Net.Mime.ContentType ct = new System.Net.Mime.ContentType(System.Net.Mime.MediaTypeNames.Text.Plain);
-                    Attachment attach = new Attachment(ms, ct);
-                    attach.ContentDisposition.FileName = file.AbsolutePath.Substring(file.AbsolutePath.LastIndexOf('\\') + 1) + ".signed";
-
+                    var ct = new System.Net.Mime.ContentType(System.Net.Mime.MediaTypeNames.Text.Plain);
+                    var attach = new Attachment(ms, ct);
+                    attach.ContentDisposition.FileName = file.AbsolutePath.Substring(file.AbsolutePath.LastIndexOf('\\') + 1);
+                    //attach.ContentDisposition.FileName = file.AbsolutePath.Substring(file.AbsolutePath.LastIndexOf('\\') + 1) + ".signed";
 
 
                     myMail.Attachments.Add(attach);
 
+
+                    var ms2 = new MemoryStream();
+                    byte[] encryptedHash = GetSignedHash(data, cert.GetRSAPublicKey());
+                    ms2.Write(encryptedHash, 0, encryptedHash.Length);
+                    ms2.Position = 0;
+                    var ct2 = new System.Net.Mime.ContentType(System.Net.Mime.MediaTypeNames.Text.Plain);
+                    var attach2 = new Attachment(ms2, ct2);
+                    attach2.ContentDisposition.FileName = "sign";
+
+                    myMail.Attachments.Add(attach2);
+
                     smtp.Send(myMail);
 
                     ms.Close();
+                    ms2.Close();
                 }
                 else
                     smtp.Send(myMail);
@@ -128,23 +339,21 @@ namespace ArchitectZad1WPF
                         try
                         {
                             MessageBox.Show($"Найдено письмо от {login.Text}\r\nТекст письма: {email.Text}");
-                            var attachment = email.Attachments.FirstOrDefault();
+                            var attachment = email.Attachments.FirstOrDefault(a => a.FileName != "sign");
+                            var signAttachment = email.Attachments.FirstOrDefault(a => a.FileName == "sign");
                             if (attachment != null)
                             {
-                                Cryption cr = new(recp.Text);
-                                var cert = cr.MyCertificate();
+                                var cert = GetCertificateFromStore(recp.Text);
 
-                                var signed_and_coded_data = attachment.Data;
-                                var signed_and_decoded_data = cr.RemovePkcsHeaders(signed_and_coded_data);
-                                signed_and_decoded_data = cr.Base64Decode(signed_and_decoded_data);
+                                var checkResult = CheckSignWithPublicKey(attachment.Data, signAttachment.Data, cert.GetRSAPrivateKey());
 
-                                var unsigned_data = cr.Unsign(signed_and_decoded_data);
+                                MessageBox.Show(checkResult?"Оригинальность файла подтверждена!":"Авторство НЕ подтверждено...");
 
                                 var dialog = new SaveFileDialog();
                                 dialog.FileName = attachment.FileName.Remove(attachment.FileName.LastIndexOf('.'));
                                 if (dialog.ShowDialog() == true)
                                 {
-                                    File.WriteAllBytes(dialog.FileName, unsigned_data); 
+                                    File.WriteAllBytes(dialog.FileName, attachment.Data); 
                                     MessageBox.Show("Расшифровано и сохранено успешно!");
                                 }
                             }
